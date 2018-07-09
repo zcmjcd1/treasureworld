b6d5631b1b3b2baa2d712e8a77b7ba0a9ffd7dfd
ChangmingZhao <zhaocm34@chinaunicom.cn>
2018-07-09 17:04:01 +0800

    new comit

--- pages/add_treasure/add_treasure.js
+++ pages/add_treasure/add_treasure.js
@@ -19,7 +19,29 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    this.chooseLocation()
+  },
+  chooseLocation: function () {
+    console.log("star to chooselocation1!!!!!!!!")
+    var that = this;
+    wx.chooseLocation({
+      success: function (ret) {
+        console.log('chooseLocation', ret)
+        treasure_address = ret.address;
+        treasure_title = ret.name;
+        that.setData({
+          address: treasure_address,
+          title: treasure_title
+        })
+        geopoint = {
+          latitude: +ret.latitude, //数值
+          longitude: +ret.longitude //数值
+        }
+      },
+      cancel: function () {
+        geopoint = null;//退出之后对象清空
+      }
+    })
   },
 
   /**
@@ -27,6 +49,22 @@ Page({
    */
   onShow: function () {
   
+  },
+  //上传活动图片
+  uploadPic: function () {//选择图标
+    wx.chooseImage({
+      count: 1, // 默认9
+      sizeType: ['compressed'], //压缩图
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths
+        that.setData({
+          isSrc: true,
+          src: tempFilePaths
+        })
+      }
+    })
   },
 
   /**
